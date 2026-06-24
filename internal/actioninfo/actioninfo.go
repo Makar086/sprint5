dbac8ae341ceb3c6d99a1991009930813595a5d8
Anton Makarov <makar086@yandex.ru>
2026-06-25 01:36:34 +0500

    коммит проверка

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,35 @@
-package actioninfo
+﻿package actioninfo
+import(
+	"log"
+	
+	
+//"github.com/Yandex-Practicum/tracker/internal/personaldata"
+//"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+"github.com/Yandex-Practicum/tracker/internal/daysteps"
+//"github.com/Yandex-Practicum/tracker/internal/trainings"
+		
+)
 
 type DataParser interface {
 	// TODO: добавить методы
+
+	Parse()  daysteps.Parse()
+	
+	ActionInfo() daysteps.ActionInfo
 }
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+for _, v := range dataset{
+er:= dp.Parse(v)
+	if er!=nil{
+	log.Printf("Ошибка парсинга для элемента", er)
+			continue
+	}
+str, er:= dp.ActionInfo()
+if er!=nil{
+	log.Printf("Ошибка парсинга для элемента", er)
+			}
+return str
+}
 }
